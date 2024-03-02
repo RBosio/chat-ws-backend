30404738c606b30e833b1a2817403c76444bdbc2
Fiido <roccobosio@gmail.com>
2024-03-02 11:20:02 -0300

    user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { TypeOrmModule } from "@nestjs/typeorm"
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TypeOrmModule } from "@nestjs/typeorm"
       entities: [__dirname + "/entities/*.entity{.tx,.js}"],
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/user/user.service.ts
@@ -0,0 +1,77 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common"
+import { CreateUserDto } from "./dto/create-user.dto"
+import { UpdateUserDto } from "./dto/update-user.dto"
+import { InjectRepository } from "@nestjs/typeorm"
+import { User } from "src/entities/user.entity"
+import { Repository } from "typeorm"
+import { hash } from "bcryptjs"
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const userFounded = await this.findOneByEmail(createUserDto.email)
+    if (userFounded) {
+      throw new HttpException("email is duplicated!", HttpStatus.CONFLICT)
+    }
+
+    const user = this.userRepository.create(createUserDto)
+
+    user.password = await hash(user.password, 10)
+
+    return this.userRepository.save(user)
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find()
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        rooms: true,
+      },
+    })
+
+    if (!user) {
+      throw new HttpException("User not found", HttpStatus.NOT_FOUND)
+    }
+
+    return user
+  }
+
+  async findOneByEmail(email: string): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+      relations: {
+        rooms: true,
+      },
+    })
+
+    return user
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    })
+
+    if (!user) {
+      throw new HttpException("User not found", HttpStatus.NOT_FOUND)
+    }
+
+    const userUpdated = Object.assign(user, updateUserDto)
+
+    return this.userRepository.save(userUpdated)
+  }
+}
